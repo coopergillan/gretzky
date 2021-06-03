fc78263d15d87dc53cd2333f0a3e68940029e482
Cooper Gillan <cooper.gillan@gmail.com>
2021-06-03 21:59:18 +0000

    First pass adding main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+from datetime import timedelta
+
+
+class Task:
+    def __init__(self, task_name, active_time, idle_time):
+        self.task_name = task_name
+        self.active_time = active_time
+        self.idle_time = idle_time
+
+
+def main():
+    make_coffee = Task(
+        task_name="Make coffee",
+        active_time=timedelta(minutes=1),
+        idle_time=timedelta(minutes=4),
+    )
+
+    make_toast = Task(
+        task_name="Make toast",
+        active_time=timedelta(seconds=10),
+        idle_time=timedelta(minutes=2),
+    )
+
+    all_tasks = [make_coffee, make_toast]
+
+    longest_idle = timedelta(0)
+    for task in all_tasks:
+        if task.idle_time > longest_idle:
+            longest_idle = task
+
+    print(f"Longest idle time: {task.longest_idle}")
+    print(f"Starting task: {task.task_name}")
+
+
+if __name__ == "__main__":
+    main()
